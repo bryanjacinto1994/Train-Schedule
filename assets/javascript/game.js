66da7552f9d1b6ae140bf27ac56c0daf3f815f31
Bryan Jacinto <bryan.f.jacinto1994@gmail.com>
2019-09-16 19:15:39 -0700

    Added firebase config and intitialized the app.

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyA7juTh--pff_nL0Wg4_i85BOuuvVIRvQE",
+    authDomain: "train-schedule-1dc12.firebaseapp.com",
+    databaseURL: "https://train-schedule-1dc12.firebaseio.com",
+    projectId: "train-schedule-1dc12",
+    storageBucket: "",
+    messagingSenderId: "257312302570",
+    appId: "1:257312302570:web:ee6680341d2f553cb0fe93"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  var database = firebaseConfig.database();
+
+  
